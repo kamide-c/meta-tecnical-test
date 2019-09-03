4863ee391e07a7dbce2879535709499b1076f3de
Cesar Kamide Martino <cesar.kamide.martino@everis.com>
2019-09-03 04:28:26 -0300

    mockup view completed

--- src/app/modules/mockup/mockup.module.ts
+++ src/app/modules/mockup/mockup.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MockupComponent } from './mockup.component';
+import { AngularMaterialModule } from 'src/app/modules/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [MockupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class MockupModule { }
