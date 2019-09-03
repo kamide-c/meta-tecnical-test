96894e986351aa1412f46bbe243eaca9346b649a
Cesar Kamide Martino <cesar.kamide.martino@everis.com>
2019-09-03 03:52:43 -0300

    create module mockup

---
+++ src/app/modules/mockup/mockup.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MockupComponent } from './mockup.component';
+
+@NgModule({
+  declarations: [MockupComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MockupModule { }
